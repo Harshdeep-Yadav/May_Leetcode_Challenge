d3a650027550ba7ebc3613f07130c9687130dbb2
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-05-03 13:07:51 +0530

    Solved

---
+++ Find the Difference of Two Arrays.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+       set<int>s1(nums1.begin(),nums1.end());
+       set<int>s2(nums2.begin(),nums2.end());
+        vector<vector<int>>utt={{},{}};
+        for(auto it : s1) if(s2.count(it)==0) utt[0].push_back(it);
+        for(auto it : s2) if(s1.count(it)==0) utt[1].push_back(it);
+        return utt;
+    }
+};
+
+// T.C->   O(N*logn)
