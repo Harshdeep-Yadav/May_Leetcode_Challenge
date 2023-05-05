c6cf5c33f99b9e164980b71644fbf3b41ac0b030
Harshdeep Yadav <harshdeepy5708@gmail.com>
2023-05-05 22:54:20 +0530

    Solved

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxVowels(string st, int k) {
+        int n =st.size();
+        int maxcount=0;
+        int cnt=0;
+        int i=0,j=0;
+        
+        while(j<n){
+          if(st[j]=='a'||st[j]=='e'||st[j]=='i'||st[j]=='o'||st[j]=='u'){
+                cnt++;
+              }
+          if(j-i==k){
+            if(st[i]=='a'||st[i]=='e'||st[i]=='i'||st[i]=='o'||st[i]=='u'){
+                cnt--;
+            }
+            i++;
+          }
+            maxcount=max(maxcount,cnt);
+            j++;
+            
+        }
+        return maxcount;
+    }
+};
